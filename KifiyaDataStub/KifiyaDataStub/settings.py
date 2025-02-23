4518b32dc578fcb18cf6b560a6404c4b47dfe82d
yarie4481 <153584832+yarie4481@users.noreply.github.com>
2025-02-23 06:39:13 -0800

    fix browser blocked  issue

--- KifiyaDataStub/KifiyaDataStub/settings.py
+++ KifiyaDataStub/KifiyaDataStub/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # Make sure this is before CsrfViewMiddleware
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
